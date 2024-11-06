1d52f4b21745e13e83d8ccda8f8db7190f29ede3
Akshay Gatkal <akshaytax2014@gmail.com>
2024-11-06 15:59:10 +0530

    hy

--- sample.py
+++ sample.py
@@ -1,3 +1,11 @@
-print("Hello World")
-print("NewCode")
-print("Candlestick")
+# python basic operators are there to help
+# Operator Operation Example Evaluates to…
+# ** Exponent 2 ** 3 8
+# % Modulus/remainder 22 % 8 6
+# // Integer division/floored quotient 22 // 8 2
+# / Division 22 / 8 2.75
+# * Multiplication 3 * 5 15
+# - Subtraction 5 - 2 3
+# + Addition 2 + 2 4
+mv = memoryview(bytearray(5))
+print(mv)
